5b31db0af067ed5d96b34ca7878d700f87e5a6d9
Saurabh Dabas <developerdabas@gmail.com>
2022-05-05 10:41:57 +0530

    Set-up Mongoose db connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,12 +1,10 @@
-import mongoose from 'mongoose'
-export default connectDB = () => {
+const mongoose = require('mongoose');
+const connectDB = () => {
   mongoose.Promise = global.Promise
   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false } )
-  mongoose.connection.on('error', () => {
-  throw new Error(`unable to connect to database: ${mongoUri}`)
-  })
-  .then(() => console.log("Database connected!"))
+  useUnifiedTopology: true})
+  .then(() => console.log(`Database connected!:${process.env.MONGO_URI}`))
   .catch(err => console.log(`Error:${err} & unable to connect to database: ${process.env.MONGO_URI}`));
 }
+module.exports = connectDB;
+
