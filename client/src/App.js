6962ae2ed541c0c68971279b51c2db3accf9738d
Saurabh Dabas <developerdabas@gmail.com>
2022-05-06 05:18:45 +0530

    Import Login and Dashboard Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
 function App() {
   return (
     <div className="App">
       <Login/>
+      <Dashboard/>
     </div>
   );
 }
