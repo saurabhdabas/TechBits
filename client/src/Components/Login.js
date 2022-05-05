eb64f19169fc5d5223104c573d2082e156e1a8a0
Saurabh Dabas <developerdabas@gmail.com>
2022-05-06 01:53:04 +0530

    Add Login Component

---
+++ client/src/Components/Login.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Box,Button} from '@mui/material';
+import GoogleIcon from '@mui/icons-material/Google';
+
+const Login = () => {
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: '#F3F6F9'
+      }}
+      display='flex'
+      flexDirection={'column'}
+    >
+      <Box sx={{ width:'30vw', height:'50vh',m: "auto" }} display='flex' flexDirection='column' justifyContent='space-evenly' alignItems='center'>
+        <img src="logo.png" height="200" alt="company logo"/>
+        <Button sx={{ backgroundColor: '#DC3545' }} startIcon={<GoogleIcon />}variant="contained">
+          Login With Google
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default Login
