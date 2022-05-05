e5558eb4155b6b924d4cd4d92c923a59aec72259
Saurabh Dabas <developerdabas@gmail.com>
2022-05-05 10:42:36 +0530

    import mongoose and complete db connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const connectDB = require('./configs/db');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+connectDB();
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
