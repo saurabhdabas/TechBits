52452795abb3826b7c15103be52836c5288186f5
Saurabh Dabas <developerdabas@gmail.com>
2022-05-06 05:38:37 +0530

    Add Routes for login and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
             <Login/>
           }
         />
-        <Route path="/" 
+        <Route path="/dashboard" 
           element={
             <Dashboard/>
           }
