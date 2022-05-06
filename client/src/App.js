0bb8427fbf5707977a6e59f5cc902098f9c0452f
Saurabh Dabas <developerdabas@gmail.com>
2022-05-06 05:38:01 +0530

    Add Routes for login and dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
 import Dashboard from './Components/Dashboard';
 import Login from './Components/Login';
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <Dashboard/>
+      <Routes> 
+        <Route path="/" 
+          element={
+            <Login/>
+          }
+        />
+        <Route path="/" 
+          element={
+            <Dashboard/>
+          }
+        />  
+      </Routes>
     </div>
   );
 }
