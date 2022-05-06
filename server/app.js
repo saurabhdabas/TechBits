86612ca0a5beedff4c7fca6cdba9a2a6216cbfa5
Saurabh Dabas <developerdabas@gmail.com>
2022-05-06 06:24:06 +0530

    Initialize passport.js and cookie-session

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const passport = require('passport');
 const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
 const logger = require('morgan');
 
 const connectDB = require('./configs/db');
@@ -8,9 +11,27 @@ const connectDB = require('./configs/db');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
+
 connectDB();
 
 const app = express();
+
+
+app.use(cookieSession({
+  name:'session',
+  keys:['techbits'],
+  maxAge: 24 * 60 * 60 * 100
+}))
+
+// Initialize the passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+  origin:"http://localhost:3000",
+  methods:[GET, POST, PUT ,DELETE],
+  credentials:true 
+}))
 
 app.use(logger('dev'));
 app.use(express.json());
